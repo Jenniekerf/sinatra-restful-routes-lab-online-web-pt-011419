cbe4cc270fad16e81928a0d9003d7cc0c00d9c55
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-25 20:57:40 +0000

    Automatically backed up by Learn

--- db/migrate/20190425205427_create_recipes.rb
+++ db/migrate/20190425205427_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name 
-      t.string :ingredients
+      t.text :ingredients
       t.integer :cook_time
     end
   end
